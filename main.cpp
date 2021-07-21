0935bdc7821c55b5b2c1a170989639111eb90ba1
Hemrajaher <87711639+Hemrajaher@users.noreply.github.com>
2021-07-21 10:36:44 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+ string name, lastname;
+ int Number =30, nextnumber =30, total;
+ int age;
+ name = "nature";
+ lastname = "hemraj";
+total= Number + nextnumber;
+cout << "addition of numbers  " << total << endl;
+// //cout << " name is 99.99@" << name << endl;
+// //cout << " well come to coading" << lastname << endl;
+// cout << " hemraj aher " << endl;
+// cout << " from nashik" << endl;
+// cout << " India " << name << endl;
+// cout << " My new project" << endl;
+// cout << " programming is simple thing, if you like to learn new things." << endl;
+// //cout << " Well as you know I am " << name << endl;
+// cout << " Thank you Gitanjali by " << name << endl;
+// cout << " Today I learn " << endl; cout << " hi! "; cout << "How are you? "; cout << "I hope you are doing well.";
+// cout << "I am learning c++ \n";
+// cout << " hemraj \n\n";
+// cout << "hi hemraj";
+// cout << " you want to learn";
+// cout << " c++ \t";
+// cout << "99.99";
+
+
+    //return 0;
+}
